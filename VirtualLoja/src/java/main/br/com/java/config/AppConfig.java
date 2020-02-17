d55de8bda027a0d524ed64320bb51b9c98fae90d
Diego <Diego@192.168.1.6>
2020-02-16 23:28:06 -0300

    criação da classe de configuração de iniciação

---
+++ VirtualLoja/src/java/main/br/com/java/config/AppConfig.java
@@ -0,0 +1,71 @@
+package br.com.java.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.google.common.cache.CacheBuilder;
+
+import br.com.java.controller.HomeController;
+
+
+
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses={HomeController.class})
+public class AppConfig extends WebMvcConfigurerAdapter {
+	
+	
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+	    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	    resolver.setPrefix("/WEB-INF/views/");
+	    resolver.setSuffix(".jsp");
+	    resolver.setExposedContextBeanNames("carrinhoCompras");
+	    return resolver;
+	}
+	
+	@Bean
+	public MessageSource messageSource(){
+	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+	    messageSource.setBasename("/WEB-INF/messages");
+	    messageSource.setDefaultEncoding("UTF-8");
+	    messageSource.setCacheSeconds(1);
+	    return messageSource;
+	}
+	
+	@Bean
+    public MultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+    }
+	
+	@Bean
+	public CacheManager cacheManager(){
+	  CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder().maximumSize(100).expireAfterAccess(5, TimeUnit.MINUTES);
+	  GuavaCacheManager manager = new GuavaCacheManager();
+	  manager.setCacheBuilder(builder);
+	  return manager;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	    configurer.enable();
+	}
+}
